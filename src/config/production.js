1e2bf7e29f3388feadab22d615617b1225c71f34
Dungeon_Master <mern.fullstack@gmail.com>
2018-06-05 00:22:22 +0530

    flow completed

--- src/config/production.js
+++ src/config/production.js
@@ -1,6 +1,9 @@
 const CONFIG = {
-  BASE_URL:                  'https://reqres.in',
-  LOGIN:                     '/api/login'
+  BASE_URL:                  'https://serene-cliffs-43081.herokuapp.com',
+  LOGIN:                     '/user/login',
+  SIGNUP:                     '/user/signup',
+  BLOG:                       '/blog',
+  POST:                       '/blog'
 };
 
 export default CONFIG;
